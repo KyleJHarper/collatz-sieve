46eb376a7ebe6ad812560764f8504dbdc4436c52
Kyle Harper <KyleJHarper@gmail.com>
2025-06-13 14:44:15 -0500

    Saving changes.

--- tools/single_collatz.py
+++ tools/single_collatz.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 import sys
+from lib.collatz import Collatz
 
 
-# Helper to calculate collatz as a string.
-def generate_collatz(num: int) -> str:
-    stops = [str(num)]
-    while num > 1:
-        if num % 2 == 0:
-            num = int(num / 2)
-        else:
-            num = num * 3 + 1
-        stops.append(str(num))
-    result = ', '.join(stops)
-    return result
-
-
-print(generate_collatz(int(sys.argv[1])))
+sequence = Collatz(int(sys.argv[1]), step=16)
+print(', '.join([str(s) for s in sequence.sequence]))
+print(f"It's W16 prefix is: {sequence.oe_prefix}")
