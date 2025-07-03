d7d5796c578a78d11751bdc8e4fc2616abaac125
Kyle Harper <KyleJHarper@gmail.com>
2025-07-03 16:41:29 -0500

    Adding memoizing for the Collatz properties.

--- tools/lib/collatz.py
+++ tools/lib/collatz.py
@@ -9,12 +9,33 @@ from typing import Self
 
 class Collatz:
     @property
+    def high_water_mark_index(self):
+        """ The index of the `self.sequence` where the first stop goes below `self.initial_value`. """
+        if self._high_water_mark_index == 0:
+            i = 0
+            while i < len(self._sequence):
+                i += 1
+                if self._sequence[i] < self._initial_value:
+                    self._high_water_mark_index = i
+                    break
+        return self._high_water_mark_index
+
+    @property
     def initial_value(self):
         return self._initial_value
 
     @property
-    def oe_string(self):
-        return self._oe_string
+    def oe_pattern(self):
+        if self._oe_pattern == '':
+            for stop in self.sequence:
+                self._oe_pattern += 'E' if stop % 2 == 0 else 'O'
+        return self._oe_pattern
+
+    @property
+    def fg_pattern(self):
+        if self._fg_pattern == '':
+            self._fg_pattern = Collatz.generate_fg_pattern(self._sequence)
+        return self._fg_pattern
 
     @property
     def sequence(self):
@@ -33,106 +54,23 @@ class Collatz:
         return self._step
 
     @property
-    def oe_prefix(self):
-        if self._oe_prefix is None:
-            self._oe_prefix = self.find_oe_prefix()
-        return self._oe_prefix
-
-    @property
-    def oe_prefix_3s_value(self):
-        return pow(3, self.oe_prefix.count('O'))
-
-    @property
-    def oe_prefix_2s_value(self):
-        return pow(2, self.oe_prefix.count('E'))
-
-    @property
     def up_down_string(self):
-        return self.oe_string.replace('E', 'd').replace('O', 'i')[0:-1]
+        return self.oe_pattern.replace('E', 'd').replace('O', 'i')[0:-1]
 
     def __init__(
         self,
         initial_value: int = 0,
-        step: int = 1,
     ):
         self._initial_value = initial_value
-        self._step = step
         self._sequence = Collatz.generate_sequence(initial_value)
         self._stop_count = len(self._sequence)
-        self._oe_string = ''
-        for stop in self.sequence:
-            self._oe_string += 'E' if stop % 2 == 0 else 'O'
-        self._oe_prefix = None
+        self._oe_pattern = ''
+        self._fg_pattern = ''
+        self._high_water_mark_index = 0
 
     def __str__(self):
         """ Our to-string method of a collatz should, reasonably, be the initial value of its sequence. """
         return str(self.initial_value)
-
-    def find_oe_prefix(
-        self,
-        step: int = None,
-        max_count: int = 50,
-        messages: bool = False,
-    ) -> str:
-        """
-        Starts at `self.initial_value` and creates a series of sequences by, adding `self.step` each
-        time or `step` if provided.  Will generate `count` sequences.  If `messages` is enabled,
-        additional info will be emitted whenever a sequence has to be removed for terminating before
-        the pattern is finished repeating.
-
-        Returns the prefix of Odds and Evens that match the whole series.
-        """
-        # Generate series of sequences.
-        sequences: List[Collatz] = []
-        count = 0
-        chosen_step = step if step is not None else self.step
-        while count < max_count:
-            count += 1
-            iv = self.initial_value + (chosen_step * (count - 1))
-            sequences.append(Collatz(iv))
-        # Determine how much of the string repeats.
-        index = 0
-        done = False
-        remove_index = None
-        while not done:
-            # Remove any items we flagged earlier.
-            if remove_index is not None:
-                sequences.pop(remove_index)
-                remove_index = None
-            # If we find a sequence longer than the exemplar (item 0) can test, remove it.
-            if index >= len(sequences[0].oe_string):
-                if messages:
-                    print(f"Need to remove {sequences[0].initial_value} because it's OE pattern ends before repetition ends.")
-                remove_index = 0
-                continue
-            new_char = sequences[0].oe_string[index]
-            for i, sequence in enumerate(sequences):
-                # If the sequence ends before our pattern ends, remove it.
-                if index >= len(sequence.oe_string):
-                    if messages:
-                        print(f"Need to remove {sequence.initial_value} because it's OE pattern ends before repetition ends.")
-                    remove_index = i
-                    continue
-                if sequence.oe_string[index] != new_char:
-                    done = True
-                    break
-            index += 1
-        index -= 1
-        prefix = sequences[0].oe_string[0:index]
-        if not Collatz.odd_even_alternates(sequences=sequences, index=index):
-            raise "All sequences should go between odd and even after a matching prefix ..."
-        return prefix
-
-    def is_below_high_water_mark(self) -> bool:
-        """
-        Determines if the Odd-Even prefix will bring this number below the high-water mark.
-        """
-        prefix = self.find_oe_prefix()
-        numerator = pow(3, prefix.count("O"))
-        denominator = pow(2, prefix.count("E"))
-        if numerator < denominator:
-            return True
-        return False
 
     def aligns_at(self, other_sequence: Self) -> int:
         """
@@ -176,8 +114,22 @@ class Collatz:
             return False
         current_oe = 'n/a'
         for sequence in sequences:
-            new_oe = sequence.oe_string[index]
+            new_oe = sequence.oe_pattern[index]
             if current_oe == new_oe:
                 return False
             current_oe = new_oe
         return True
+
+    @classmethod
+    def generate_fg_pattern(cls, sequence: list) -> str:
+        """ Takes a sequence and creates the f-g pattern from it. """
+        i = 0
+        pattern = ''
+        while i < len(sequence):
+            if sequence[i] % 2 == 0:
+                pattern += 'g'
+                i += 1
+            else:
+                fg_pattern += 'f'
+                i += 2
+        return pattern
--- tools/single_collatz.py
+++ tools/single_collatz.py
@@ -3,6 +3,8 @@ import sys
 from lib.collatz import Collatz
 
 
-sequence = Collatz(int(sys.argv[1]), step=16)
+sequence = Collatz(int(sys.argv[1]))
 print(', '.join([str(s) for s in sequence.sequence]))
-print(f"It's step count (stopping time) is: {sequence.stopping_time}")
+print(f"Its step count (stopping time) is: {sequence.stopping_time}")
+print(f"OE Pattern: {sequence.oe_pattern}  ({len(sequence.oe_pattern)})")
+print(f"FG Pattern: {sequence.fg_pattern}  ({len(sequence.fg_pattern)})")
