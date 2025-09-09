18988e932ab5520eef34af5f8a8a3318f28a086d
Kyle Harper <KyleJHarper@gmail.com>
2025-09-09 14:18:49 -0500

    Adding a watcher for progress files.

---
+++ watch_progress.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function get_latest {
+  tail -n 1 "${file}" | awk '{print $3}'
+}
+
+
+file="${1}"
+if [ ! -f "${file}" ] ; then
+  echo "Cannot find file: ${file}" >&2
+  exit 1
+fi
+
+start=$(get_latest)
+current=0
+delta=0
+rate=0
+while true ; do
+  sleep 5
+  current=$(get_latest)
+  delta=$((current - start))
+  rate=$((delta / SECONDS))
+  echo "Current: ${current}   Rate: ${rate}/s   Total Processed: ${delta}"
+done
+
